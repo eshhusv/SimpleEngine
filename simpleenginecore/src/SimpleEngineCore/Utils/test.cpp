87f63ee9ba5077cb7f9659119cdbd05b827ce4f6
eshhusv <ak7527203@gmail.com>
2023-04-15 20:10:44 +0200

    Modified hierarchy

---
+++ simpleenginecore/src/SimpleEngineCore/Utils/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "SimpleEngineCore/Utils/test.hpp"
+
+namespace SimpleEngine {
+	
+	void sayHello()
+	{
+		std::cout << "Hello from Simple Engine Core" << std::endl;
+	}
+}
---
+++ simpleengineeditor/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <SimpleEngineCore/Utils/test.hpp>
+
+int main()
+{
+	std::cout << "Hello, World" << std::endl;
+
+	SimpleEngine::sayHello();
+
+	return 0;
+}
